541700d46061ec0d8c4ec3e33205589373620afe
孙春晓 <15537791297@qq.com>
2018-08-08 10:48:59 +0800

    8.7 首页列表标签1

--- src/views/details/evaluating.vue
+++ src/views/details/evaluating.vue
@@ -292,7 +292,7 @@
 				// console.log(_width,_width1)
 
 				if(_width<1590){
-					var left = _width1+650
+					var left = _width1+645
 					$(".common-attention").css("left",left)
 				}else{
 					var left = _width1+715
